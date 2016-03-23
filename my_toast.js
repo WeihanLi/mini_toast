0077d4f3c154a8e31d2fc2e5f1d9593241c8b4b0
WeihanLi <ben121011@126.com>
2016-03-23 20:40:48 +0800

    my_toast.js 代码优化

--- my_toast.js
+++ my_toast.js
@@ -1,46 +1,57 @@
 function Toast(options) {
     //default settings
-    this._settings = {root:"my_toast",autoHide:true,delay:2000,content:"这里是toast内容",callback:function(){}};
+    this._settings = {root:"my_toast",logger:false,autoHide:true,delay:2000,content:"这里是toast内容",callback:function(){}};
     //custom settings
     this._customSettings = {};
-    
+    //define the function getSetting
     this.getSetting = function (settingName){
         return this._settings[settingName];
-    }
-    
+    };
+    //define the function setSetting
     this.setSetting = function (settingName,settingValue){
         this._settings[settingName] = settingValue;
-    }
-    
-    this.initToast = function (options){
-        this._customSettings = options;        
-        for (var settingName in this._customSettings) {
-            this._settings[settingName] = (this._customSettings[settingName] == undefined) ?  this._settings[settingName] : this._customSettings[settingName];
-        }               
-        //console.log('toast inited');
     };
-    
-    this.hide = function (){   
-        //console.log("hide begin");
-        var ele = document.getElementById(this.getSetting('root'));  
-        if (ele.style.display == 'none') {
-            //console.warn('already hide');
+    //toast init
+    this.initToast = function (options){
+        this._customSettings = options;
+        for (var settingName in this._customSettings) {
+            if (this._customSettings.hasOwnProperty(settingName)) {
+                this._settings[settingName] = (this._customSettings[settingName] === undefined) ?  this._settings[settingName] : this._customSettings[settingName];
+            }
+        }
+        if(this.getSetting("logger")){
+            console.log('toast inited');
+        }
+    };
+    //hide toast
+    this.hide = function (){
+        if(this.getSetting("logger")){
+            console.log("hide begin");
+        }
+        var ele = document.getElementById(this.getSetting('root'));
+        if (ele.style.display === 'none') {
+            if(this.getSetting("logger")){
+                console.warn('already hide');
+            }
             return;
         }
         ele.style.display = 'none';
-        if(typeof arguments[0] == 'function'){
+        if(typeof arguments[0] === 'function'){
             arguments[0]();
         }
-        //console.log("hide end");
-    }; 
+        if(this.getSetting("logger")){
+            console.log("hide end");
+        }
+    };
     
     /*
+    *show toast
     *show()
     *show(content)
     *show(delay)
     *show(content,delay)
     *show(content,callback)
-    *show(content,autoHide) 
+    *show(content,autoHide)
     *show(content,delay,callback)
     *show(content,autoHide,delay,callback) //autoHide为false时设置delay是没有什么意义的
     */
@@ -49,55 +60,60 @@ function Toast(options) {
         var delay = this.getSetting('delay');
         var autoHide = this.getSetting('autoHide');
         var callback = this.getSetting('callback');
-        // console.log("show toast begin");
-        if (arguments.length==1) {
-            if(typeof arguments[0]=='string'){
-                content = arguments[0];    
-            }else if(typeof arguments[0]=='number'){
+        if(this.getSetting("logger")){
+            console.log("show toast begin");
+        }
+        if (arguments.length===1) {
+            if(typeof arguments[0]==='string'){
+                content = arguments[0];
+            }else if(typeof arguments[0]==='number'){
                 delay = arguments[0];
-            }else if(typeof arguments[0] == 'function'){
+            }else if(typeof arguments[0] === 'function'){
                 callback = arguments[0];
-            }else if(typeof arguments[0] == 'boolean'){
+            }else if(typeof arguments[0] === 'boolean'){
                 autoHide = arguments[0];
-            }           
-        }else if(arguments.length==2){
+            }
+        }else if(arguments.length===2){
             //第一个参数
-            if(typeof arguments[0] == 'string'){
-                content = arguments[0];    
-            }else if(typeof arguments[0] == 'number'){
+            if(typeof arguments[0] === 'string'){
+                content = arguments[0];
+            }else if(typeof arguments[0] === 'number'){
                 delay = arguments[0];
-            }else if(typeof arguments[0] == 'boolean'){
+            }else if(typeof arguments[0] === 'boolean'){
                 autoHide = arguments[0];
             }
             //第二个参数
-            if(typeof arguments[1] == 'number'){
+            if(typeof arguments[1] === 'number'){
                 delay = arguments[1];
-            }else if(typeof arguments[1] == 'function'){
+            }else if(typeof arguments[1] === 'function'){
                 callback = arguments[1];
             }
-        }else if(arguments.length==3){
+        }else if(arguments.length===3){
             content = arguments[0];
             delay = arguments[1];
             callback = arguments[2];
         }
-        
         var body = document.getElementsByTagName("body")[0];
         var ele = document.getElementById(this.getSetting('root'));
-        if(typeof ele == 'undefined'||ele == null){
+        if(typeof ele === 'undefined'||ele === null){
             ele = document.createElement("div");
             ele.innerHTML = '<div class="my_mask_transparent"></div><div class="my_toast"><p class="my_toast_content">'+content+'</p></div>';
             ele.style.display = 'none';
             ele.id = this.getSetting('root');
-            body.appendChild(ele);    
+            body.appendChild(ele);
         }else{
             ele.innerHTML = '<div class="my_mask_transparent"></div><div class="my_toast"><p class="my_toast_content">'+content+'</p></div>';
         }
-        //注册隐藏事件
+        //register hide event
         var mask = document.getElementsByClassName("my_mask_transparent")[0];
-        mask.onclick = function(){ ele.style.display = 'none'; callback(); };
-                        
-        if (ele.style.display!='none') {
-            //console.warn('already shown');
+        mask.onclick = function(){
+            ele.style.display = 'none';
+            callback();
+        };
+        if (ele.style.display!=='none') {
+            if(this.getSetting("logger")){
+                console.warn('already shown');
+            }
             return;
         }
         ele.style.display = 'block';
@@ -107,24 +123,26 @@ function Toast(options) {
                 callback();
             },delay);
         }
-        // console.log("show toast end");
+        if(this.getSetting("logger")){
+            console.log("show toast end");
+        }
     };
-    
+    //toogle
     this.toogle = function(){
         var ele = document.getElementById(this.getSetting('root'));
-        if(typeof ele == 'undefined' || ele == null || ele.style.display=='none'){
+        if(typeof ele === 'undefined' || ele === null || ele.style.display === 'none'){
             this.show(arguments);
         }else{
             this.hide();
         }
     };
-    
+    //constructor
     constructor = this.initToast(options);
-    
-    if (Toast._initialized == "undefined") {
-        Toast.prototype.constructor = this.initToast(options);           
-        
+    //make sure there is a constructor
+    if (Toast._initialized === "undefined") {
+        Toast.prototype.constructor = this.initToast(options);        
         Toast._initialized = true;
     }
-}    
+}
+//point a toast object to window.toast
 window.toast = new Toast();
